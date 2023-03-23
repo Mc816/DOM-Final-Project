a1b33603a96c73f171176f6844bef8b39b75da77
Michael Charime <Michaelcharime@gmail.com>
2023-03-22 19:48:40 -0700

    Updates

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log(window.location.href);
+
 class BabyYoda {
   constructor(happines, tiredness) {
     this.happines = happines;
@@ -14,27 +16,121 @@ class Doggie {
 }
 
 const babaYoda = new BabyYoda("happy", "tired");
-//console.log(babaYoda);
-const pullto = new Doggie("Sad", "mad", "angry");
+console.log(babaYoda.happines);
+const pullto = new Doggie("Sad", "mad", 6);
 
 //put if else statments based on the button clicked
 
 //Maybe loop through it and
 //First image button
 
+//const pEl = document.createdElement("p");
+
+//if statment here for the picuteres
+
 const imgButton1 = document.getElementById("button1");
 const myImage1 = document.getElementById("img1");
-imgButton1.addEventListener("click", () => {
-  myImage1.classList.toggle("none");
+const pTag = document.querySelector(".p2");
+//const pTag2 = document.querySelector(".p2");
+const link = document.getElementById("popupbttn");
+const linkBttn = document.querySelector("#nextbtn");
+//const clicked = true;
+const text = {
+  "/pages/page1.html": `Baby Yoda is Super ${babaYoda.happines}`,
+  "/pages/page4.html": `This is page 4${babaYoda.happines}`,
+};
 
-  imgButton1.textContent = <p>`Baby Yoda is super ${this.happines}`</p>;
-  // const pEl = document.getElementById("p1");
-  /*pEl.innerHTML = `Baby Yoda is ${this.happines}`;
-  pEl.classList.toggle("p1"); */ // not selected to a click or event so it shows up but doesnt
+const text2 = {
+  "/pages/page1.html": `baby yoda`,
+};
+imgButton1.addEventListener("click", () => {
+  let url = window.location.pathname;
+  linkBttn.className = "";
+  myImage1.className = "";
+
+  if (linkBttn2 === null) {
+    //make sure there are no other classes in the class nsme
+    linkBttn.className = "";
+  } else {
+    linkBttn2.className = "disable";
+    linkBttn.className = "";
+  }
+
+  myImage2.className = "disable"; //Changing the class on both event listners, but they do oppiste things
+  //imgButton1.classList.toggle("noBttn");
+  pTag.textContent = text[url];
+  pTag.className = "";
 });
 
 const imgButton2 = document.getElementById("button2");
+const pTag3 = document.querySelector(".gone2");
 const myImage2 = document.getElementById("img2");
+const linkBttn2 = document.querySelector("#nextbtn2");
 imgButton2.addEventListener("click", () => {
-  myImage2.classList.toggle("none");
+  linkBttn.className = "none";
+  myImage1.className = "none";
+
+  let url = window.location.pathname;
+  myImage2.className = "";
+  //I need to fix the issue with the other qoute pop-ing up
+  if (linkBttn2 === null) {
+    //make sure there are no other classes in the class nsme
+    linkBttn.className = "";
+  } else {
+    linkBttn.className = "disable";
+    linkBttn2.className = "";
+  }
+
+  pTag3.textContent = `Baby Yoda is Super ${babaYoda.tiredness} he is ready to go to bed`; //class for baby yoda going to be
+  pTag3.classList.toggle("gone2");
 });
+/*
+imgButton1.addEventListener("Click");
+
+$("#button1").click(function () {
+  ev.preventDefault();
+  $("#popupbttn").removeClass("popupbttn1");
+  $("#popupbttn1").addClass("popupbttn1");
+});
+/* Might be able to use this 
+imgButton1.addEventListener("click", () => {
+  return link.classList.toggle("linkclass");
+});
+*/
+
+// pTag.classList.toggle("gone");
+
+// pTag2.textContent = `Pulto food level is ${pullto.hungry} `;
+// pTag2.classList.toggle("p2");
+
+/*if (imgButton1 === clicked) {
+    imgButton2.classList.remove("noBttn");
+  }*/
+/*
+const link = document.getElementById("link");
+function linkForAllLinks() {
+  if(displayValue = "")
+  link.style.display === "";
+  displayValue = "none";
+
+  link.style.display = displayValue;
+}
+
+if (imgButton1 === true) {
+}
+*/
+//imgButton1.addEventListener("click");
+//pTag.classList.toggle("gone");
+//,{ once: true }
+
+/*
+function rest() {
+  var url = document.getElementById("mainUrl").value;
+  document.write("Going back");
+  setTimeout(function () {
+    window.location = mainUrl;
+  }, 3000);
+}
+
+rest(url);
+*/
